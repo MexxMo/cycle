7792ec9220882d1b381d12f5a8d66b39dc584a95
Ildar <mexx0174@gmail.com>
2022-12-08 13:02:12 +0500

    Задачи 1-4

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+
+        for (int i = 1; i <= 10; i = i + 1) {
+
+            System.out.println(i);
+        }
+        System.out.println("Задача 2");
+
+        for (int i = 10; i >= 1; i = i - 1) {
+            System.out.println(i);
+        }
+
+        System.out.println("Задача 3");
+
+        for (int x = 0; x <= 17; x = x + 2) {
+            System.out.println(x);
+        }
+
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i = i - 1) {
+            System.out.println(i);
+        }
+
+
+
+
+    }
+}
